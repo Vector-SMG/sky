2eea3100bc278ed3298ec5cc735ebb9f5d0ebdf0
Cornucopia <13040839537@163.com>
2021-03-20 16:27:29 +0800

    first commit

---
+++ src/test/java/com/cornucopia/sky/SkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cornucopia.sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
